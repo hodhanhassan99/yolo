029603b48d110b2f00837e0b4af1a5da1d02a24c
hodhanhassan99 <ajfreeforall@gmail.com>
2026-08-09 12:56:07 +0300

    Fix Stage Two deployment and Terraform state

--- Stage_two/terraform/main.tf
+++ Stage_two/terraform/main.tf
@@ -40,6 +40,13 @@ resource "aws_security_group" "yolo_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     description = "Backend API"
